038664b001ebac65b463c6a635135fbfafb2f7ca
DirsanA <dirsanantehun739@gmail.com>
2025-02-14 10:48:17 -0800

    mongodb database connected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
-
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+console.log(process.env.MONGO_URI);
 const port = 3000;
+
 app.get("/", function (req, res) {
   res.send("Hello from the port 3000");
 });
